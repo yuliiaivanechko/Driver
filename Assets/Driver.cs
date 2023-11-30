11a1a47ae66af9028bd81eac028d2d72e268f351
yuliiaivanechko <yuliaivanechko@gmail.com>
2023-11-30 09:45:49 +0200

    current work

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,8 +6,9 @@ public class Driver : MonoBehaviour
 {
 
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
-
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +22,19 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SpeedUp")
+        {
+            moveSpeed = boostSpeed;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
     }
 }
